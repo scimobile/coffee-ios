cc8aeacad0a33bdb02f015ffea544f760c29198d
s1thu <s1thu.w1n.5601@gmail.com>
2024-08-07 00:24:47 +0630

    [COF-0001] - Onboarding Screen - > Scrolalble, Button Control, PageControl,

--- Coffee/Coffee/Application/SceneDelegate.swift
+++ Coffee/Coffee/Application/SceneDelegate.swift
@@ -16,7 +16,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+
+        // Set Onboarding storyboard as initial storyboard
+        let storyboard = UIStoryboard(name: "OnBoarding", bundle: nil)
+        let mainSb = UIStoryboard.init(name: "Main", bundle: nil)
+        var rootVC:UIViewController? = nil
+        
+        if UserDefaults.standard.bool(forKey: "hasUsedAppBefore"){
+            rootVC = mainSb.instantiateViewController(withIdentifier: "MainVC") as! MainVC
+            
+        }else{
+            rootVC = storyboard.instantiateViewController(withIdentifier: "OnboardingVC") as! OnboardingVC
+        }
+
+        // Instantiate the initial view controller
+        window?.rootViewController = UINavigationController(rootViewController: rootVC!)
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
