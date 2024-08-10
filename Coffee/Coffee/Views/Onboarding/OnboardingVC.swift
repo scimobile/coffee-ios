2c8430406b12d44b69f4418f3b427d379d689cd6
s1thu <s1thu.w1n.5601@gmail.com>
2024-08-11 00:26:19 +0630

    [COF-0001] refractor using ViewModel, UserDefaultsKey, Fonts Enum

--- Coffee/Coffee/Views/Onboarding/OnboardingVC.swift
+++ Coffee/Coffee/Views/Onboarding/OnboardingVC.swift
@@ -13,73 +13,73 @@ class OnboardingVC: UIViewController {
     @IBOutlet weak var pageControl:UIPageControl!
     @IBOutlet weak var btnNext:UIButton!
     
-    let pages: [OnboardingPage] = [OnboardingPage(title: "Welcome To \nCaffeine Corner", imageName: "welcome", description: "Discover your favorite coffee, order with ease, and enjoy exclusive perks."),
-                                   OnboardingPage(title: "Earn Loyalty \nPoints", imageName: "welcome2", description: "Collect points with every purchase and redeem them for free coffee and exclusive rewards."),
-                                   OnboardingPage(title: "Easy Ordering \n", imageName: "welcome3", description: "Order your favorite coffee in just a few taps and pick it up at your convenience.")
-    ]
+    private lazy var vm:OnboardingVM = .init(delegate: self)
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("cvOnboarding frame: \(cvOnboarding.frame)")
+        setupView()
+        setupBindings()
+    }
+    
+    private func setupView(){
+        //Collection View Binding
         cvOnboarding.register(.init(nibName: "OnboardingCell", bundle: nil), forCellWithReuseIdentifier: "OnboardingCell")
         cvOnboarding.dataSource = self
         cvOnboarding.delegate = self
         cvOnboarding.isPagingEnabled = true
-       
-        pageControl.numberOfPages = pages.count
         
+        //Fetch Data from ViewModel
+        vm.fetchData()
+        
+        pageControl.numberOfPages = vm.onboardingData.count
+        
+        btnNext.titleLabel?.font = AppFont.poppinsRegular.of(size: 12)
+    }
+    
+    private func setupBindings(){
         btnNext.addTarget(self, action: #selector(btnNextTapped), for: .touchUpInside)
-        
-        btnNext.titleLabel?.font = UIFont(name: "Poppins-Regular", size: 12)
-        
     }
     
     @objc func btnNextTapped() {
         let currentIndex = pageControl.currentPage
-        if currentIndex < pages.count - 1 {
+        if currentIndex < vm.onboardingData.count - 1 {
             let nextIdex = IndexPath(item: currentIndex + 1, section: 0)
             cvOnboarding.scrollToItem(at: nextIdex, at: .centeredHorizontally, animated: true)
             pageControl.currentPage = nextIdex.item
         } else {
-            UserDefaults.standard.set(true, forKey: "hasUsedAppBefore")
+            UserDefaults.standard.set(true, forKey: UserDefaultsKeys.hasUsedAppBefore)
             /// TODO : GO TO LOGIN
             print("Go to Login")
         }
-        
         updateBtnTitle()
     }
     
     private func updateBtnTitle() {
-        if pageControl.currentPage == pages.count - 1{
+        if pageControl.currentPage == vm.onboardingData.count - 1{
             btnNext.setTitle("Get Started", for: .normal)
         } else {
             btnNext.setTitle("Next", for: .normal)
         }
     }
   
-
 }
 
+//UICollectionViewDataSource
 extension OnboardingVC: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        pages.count
+        vm.onboardingData.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCell", for: indexPath) as? OnboardingCell
         guard let cell = cell else { return UICollectionViewCell() }
-        
-        let currentPage = pages[indexPath.item]
-        cell.title.text = currentPage.title
-        cell.ivImage.image = UIImage(named: currentPage.imageName)
-        cell.desLabel.text = currentPage.description
-        
+        cell.setup(onboardingItem: vm.onboardingData[indexPath.row])
         return cell
     }
-    
-    
 }
 
+//UICollectionViewDelegateFlowLayout
 extension OnboardingVC: UICollectionViewDelegateFlowLayout{
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -97,5 +97,12 @@ extension OnboardingVC: UICollectionViewDelegateFlowLayout{
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         pageControl.currentPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
         updateBtnTitle()
+    }
+}
+
+//OnboardingDelegate
+extension OnboardingVC: OnboardingDelegate{
+    func getOnboardingPages() {
+        cvOnboarding.reloadData()
     }
 }
--- Coffee/Models/Onboarding/OnboardingPage.swift
+++ Coffee/Models/Onboarding/OnboardingPage.swift
@@ -11,4 +11,18 @@ struct OnboardingPage {
     let title: String
     let imageName: String
     let description: String
+    
+    static func getPages() -> [OnboardingPage]{
+        return  [OnboardingPage(title: "Welcome To \nCaffeine Corner",
+                                imageName: "welcome",
+                                description: "Discover your favorite coffee, order with ease, and enjoy exclusive perks."),
+                 OnboardingPage(title: "Earn Loyalty \nPoints",
+                                imageName: "welcome2",
+                                description: "Collect points with every purchase and redeem them for free coffee and exclusive rewards."),
+                 OnboardingPage(title: "Easy Ordering \n",
+                                imageName: "welcome3",
+                                description: "Order your favorite coffee in just a few taps and pick it up at your convenience.")
+                 ]
+    }
+    
 }
