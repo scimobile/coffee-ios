bbfb185f604d8e0e8a877dae71075547f65db935
s1thu <s1thu.w1n.5601@gmail.com>
2024-08-14 03:13:08 +0630

    UIControl Finished

--- Coffee/Coffee/Views/MenuDetail/Array.swift
+++ Coffee/Coffee/Views/MenuDetail/Array.swift
@@ -5,7 +5,14 @@
 //  Created by Sithu Win on 11/08/2024.
 //
 
-import Foundation
 import UIKit
+
+extension Array where Element == UIButton{
+    func addTarget(selector:Selector){
+        self.forEach { button in
+            button.addTarget(self, action: selector, for: .touchUpInside)
+        }
+    }
+}
 
 
